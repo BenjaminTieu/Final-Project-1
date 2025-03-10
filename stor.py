51a25ef3201527cd1fd93f33a31675ea4b539346
Lukas-Watts <lwattsdi@calpoly.edu>
2025-03-09 17:33:20 -0700

    Update stor.py

--- stor.py
+++ stor.py
@@ -31,6 +31,12 @@ opening_scene_l2 = ["Yes, it's Questin' Time!!!","No, that sounds like too much 
 forest_scene_l1 = ["Go through the DARK EVIL FOREST","Return home and watch paint dry"]
 forest_scene_l2 = ["Investigate the bushes","Ignore it"]
 forest_scene_l3 = ["Short Sword","Boots"]
+#All decisions in doom_mtn_and_end
+fin_l1 =["Free Her","Leave her in the Cage","RUN!"]
+store14=["Yes","Hell No"]
+store13=["Yes","Hell No"]
+store12=["Attack","Ignore and sneak into the castle"]
+store11=["Fight through(STRENGTH)","Attempt to sneak past(AGILITY)"]
 # This is the dictionary that will be used in forest_scene
 forest_scene_dict1 = {"Short Sword": {"Name": "Short Sword",
                                 "Description": "A doubled-edged sword designed for close-quarters \n"
@@ -54,6 +60,7 @@ mustang_village_l2 = ["I long for a warm meal, surely the princess can wait a fe
 mustang_village_l3 = ["STEW","FISH"]
 mustang_village_l4 = ["Of course! Come here little dude", "No, get away from me you flea-stained rodent",
                       "Slay the adorable creature?"]
+mustang_village_l5=["HEAVY ARMOR","LIGHT ARMOR"]
 # These are the dictionaries that will be used in mustang_village
 mustang_village_dict1 = {"STEW": {"Name": "STEW",
                                   "Description": "A mystery concoction prepared by the innkeeper himself.\n"
@@ -65,3 +72,14 @@ mustang_village_dict1 = {"STEW": {"Name": "STEW",
                                   "Description": "A fish prepared by the innkeeper himself. Strange, I \n"
                                                  "don't remember there being any fishermen in this part \n"
                                                  "of town."}}
+mustang_village_dict2={"HEAVY ARMOR": {"Name": "HEAVY ARMOR",
+                                  "Description": "A Mythical Armor that is said to have been previously worn by JOE MAMA Himself!.\n"
+                                                 "That's probably the reason it is so big!\n"
+                                                 "Some say that the armor even imbues its wearers with JOE MAMA'S STRENGTH.\n"
+                                                 "But due to its weight it slows any user down heavily.",
+                                   "Health": "+40", "Strength": "+25", "Agility": "-20"},
+                         "LIGHT ARMOR": {"Name": "LIGHT ARMOR",
+                                  "Description": "Made with mythical materials from the far lands of WALMART. \n"
+                                                 "This armor is sure to provide some sort of protection. \n"
+                                                 "It also imbues the wearer with the speed of a Black Friday Walmart shopper.",
+                                         "Health": "+10", "Agility": "+20"}}
